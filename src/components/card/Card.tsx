74a925f75a7ee540a86fd153b9a8fc6354860655
Emerson Trindade <trinddade29@gmail.com>
2024-07-18 18:29:56 -0300

    feat: add props to card

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,31 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
+import { CardProps } from "./types";
 
-export const Card = () => {
+export const Card = (props: CardProps) => {
   return (
-    <Link href="/cursos/dsdlaa" className="hover:no-underline ">
+    <Link href={props.href} className="hover:no-underline ">
       <article className="flex gap-2 flex-col p-2 rounded hover:bg-primary">
         <Image
           height="0"
           width="1000"
           draggable="false"
-          alt="Curso de API Rest"
+          src={props.image}
+          alt={props.title}
           className="aspect-video object-cover rounded-2xl"
-          src="https://i.ytimg.com/vi/SVepTuBK4V0/hqdefault.jpg"
         />
 
         <h4 className="font-extrabold text-lg">
-          Curso de API Rest, Node e Typescript
+          {props.title}
         </h4>
 
         <p className="line-clamp-3">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ipsam perferendis, libero
-          voluptas porro sed numquam id delectus iste? Praesentium reiciendis nihil dicta earum laudantium
-          enim quis voluptas aliquam adipisci? Vero quasi accusantium voluptas optio sunt culpa rerum
-          dicta impedit, eligendi in aspernatur minima neque facere magnam sapiente est ullam architecto
-          tempora odit iusto harum excepturi ratione, sed recusandae. Et nulla fuga debitis ex ipsum
-          molestiae quas sequi iusto? Quae provident reiciendis suscipit ducimus praesentium voluptas
-          harum odit, quas ex eos perspiciatis cum itaque officiis pariatur quos? Aliquam, aut perspiciatis!
+          {props.description}
         </p>
       </article>
     </Link>
